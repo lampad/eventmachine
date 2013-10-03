4904b2370dfe7438ec24f1fc06204c0170878841
Matt Campbell <matthew.campbell@asynchrony.com>
2013-10-03 16:50:51 -0500

    Implement get_peer_cert

--- tests/test_ssl_methods.rb
+++ tests/test_ssl_methods.rb
@@ -1,10 +1,15 @@
 require 'em_test_helper'
 
 class TestSSLMethods < Test::Unit::TestCase
+  def setup
+      $dir = File.dirname(File.expand_path(__FILE__)) + '/'
+      $client_cert_from_file = File.read($dir+'client.crt')
+      $server_cert_from_file = File.read($dir+'server.crt')
+  end
 
   module ServerHandler
     def post_init
-      start_tls
+      start_tls(:private_key_file => $dir+'server.key', :cert_chain_file => $dir+'server.crt', :verify_peer => true)
     end
 
     def ssl_handshake_completed
@@ -14,11 +19,15 @@ class TestSSLMethods < Test::Unit::TestCase
       $server_cipher_name = get_cipher_name
       $server_cipher_protocol = get_cipher_protocol
     end
+
+    def ssl_verify_peer cert
+      true
+    end
   end
 
   module ClientHandler
     def post_init
-      start_tls
+      start_tls(:private_key_file => $dir+'client.key', :cert_chain_file => $dir+'client.crt')
     end
 
     def ssl_handshake_completed
@@ -48,9 +57,9 @@ class TestSSLMethods < Test::Unit::TestCase
     assert($server_called_back)
     assert($client_called_back)
 
-    assert($server_cert_value.is_a?(NilClass))
-    assert($client_cert_value.is_a?(String))
 
+    assert_equal($server_cert_from_file, $server_cert_value.gsub("\r", ""))
+    assert_equal($client_cert_from_file, $client_cert_value.gsub("\r", ""))
     assert($client_cipher_bits > 0)
     assert_equal($client_cipher_bits, $server_cipher_bits)
 
@@ -62,4 +71,4 @@ class TestSSLMethods < Test::Unit::TestCase
     assert_equal($client_cipher_protocol, $server_cipher_protocol)
   end
 
-end
+end if EM.ssl?
